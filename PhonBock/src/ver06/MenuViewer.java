865b510c88f3f65695008246571237c559a886ba
Hyeyum <147293737+czennie-hyeyum@users.noreply.github.com>
2023-10-18 17:32:34 +0900

    Delete PhonBock directory

--- PhonBock/src/ver06/MenuViewer.java
+++
@@ -1,9 +0,0 @@
-package ver06;
-
-public class MenuViewer {
-	public static void showMenu() { 
-		System.out.println("--------------------------");
-		System.out.println("1.입력  2.전체조회  3.종료");
-		System.out.println("--------------------------");
-	}
-}
