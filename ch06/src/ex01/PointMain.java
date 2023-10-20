568559b271be854a1d2320c464a0d8fab624476b
KH302 <KH302@DESKTOP-DDE5K7J>
2023-10-20 10:56:52 +0900

    10.20 10:56

--- ch06/src/ex01/PointMain.java
+++ ch06/src/ex01/PointMain.java
@@ -7,10 +7,12 @@ public class PointMain {
 		Point p2 = new Point(10, 10);
 		Point p3 = new Point(20, 20);
 		
+		// 객체가 같은지를 비교할 때는 .equals()를 사용
 		System.out.println(p1 == p2); // false
 		System.out.println(p1.equals(p2)); // true
 		System.out.println(p1 == p3); // false
 		System.out.println(p1.equals(p3)); // false
+		
 	}
 
 }
