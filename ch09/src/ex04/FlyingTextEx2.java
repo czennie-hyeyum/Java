7da1d28c799c6a7560e16b3cc25275f79eb9dcc1
KH302 <KH302@DESKTOP-DDE5K7J>
2023-10-24 12:29:47 +0900

    23.10.24 12:29

--- ch09/src/ex04/FlyingTextEx2.java
+++ ch09/src/ex04/FlyingTextEx2.java
@@ -8,19 +8,18 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 
 @SuppressWarnings("serial")
-public class FlyingTextEx extends JFrame {
+public class FlyingTextEx2 extends JFrame {
 	private Container con = getContentPane();
 	private JLabel label = new JLabel("Hello");
 	private static final int MOVE_UNIT = 10;
 	
-	public FlyingTextEx() {
+	public FlyingTextEx2() {
 		super("키보드로 레이블 움직이기");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setSize(500, 500);
 		setVisible(true);
 		con.setLayout(null);
 		label.setBounds(50, 50, 30, 15);
-		label.setOpaque(true);
 		con.add(label);
 		con.addKeyListener(new MyKeyAdapter());
 		
@@ -36,27 +35,47 @@ public class FlyingTextEx extends JFrame {
 			int keyCode = e.getKeyCode();
 			int x = label.getX();
 			int y = label.getY();
+			int maxX = con.getWidth() - label.getWidth();
+			int maxY = con.getHeight() - label.getHeight();
 			
+				
 			switch(keyCode) {
 			case KeyEvent.VK_UP:
-				label.setLocation(x, y - MOVE_UNIT);
+				y -= MOVE_UNIT;
+				if(y < 0) {
+					y = 0;
+				}
 				break;
+				
 			case KeyEvent.VK_DOWN:
-				label.setLocation(x, y + MOVE_UNIT);
+				y += MOVE_UNIT;
+				if(y > maxY) {
+					y = maxY;
+				}
 				break;
+				
 			case KeyEvent.VK_LEFT:
-				label.setLocation(x - MOVE_UNIT, y);
+				x -= MOVE_UNIT;
+				if(x < 0) {
+					x = 0;
+				}
 				break;
+				
 			case KeyEvent.VK_RIGHT:
-				label.setLocation(x + MOVE_UNIT, y);
+				x += MOVE_UNIT;
+				if(x > maxX) {
+					x = maxX;
+				}
 				break;
-
 			}
+			
+			label.setLocation(x, y);
+			
 		}
 	}
 	
 	public static void main(String[] args) {
-		new FlyingTextEx();
+		new FlyingTextEx2();
 	}
 
 }
