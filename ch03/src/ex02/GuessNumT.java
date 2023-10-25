b22630b593ad311f5af77735f13facdd495c2260
KH302 <KH302@DESKTOP-DDE5K7J>
2023-10-25 12:59:42 +0900

    2023.10.25 12:59

--- ch03/src/ex02/GuessNumT.java
+++ ch03/src/ex02/GuessNumT.java
@@ -9,7 +9,6 @@ public class GuessNumT {
 	public static int count = 5;
 	public static int targetNum = 0;
 	
-	// 메인 클래스
 	public static void main(String[] args) {
 		// Scanner scanner = new Scanner(System.in);
 //		int[] records = {30000, 30000, 30000, 30000, 30000};
@@ -22,49 +21,45 @@ public class GuessNumT {
 		showRule();
 		runGame();
 		
-		// 기회 소진 출력
 		System.out.println("기회를 모두 소진하였습니다.");
 		
-		// 스캐너 닫기
 		scanner.close();
-	} // main
+	} 
 	
-	// 게임 시작
 	public static void runGame() {
 		while(true) {
-			System.out.println("for"); // for 출력
+			System.out.println("for"); 
 			int userNum = inputData(); 
 			judge(targetNum, userNum);
 			count--; 
-			if (count == 0) { // 기회가 0번이 되었을 때 멈춤
+			if (count == 0) { 
 				break; 
 			}
-			System.out.println("남은 기회는 " + count + "번입니다."); // 남은 기회 출력
+			System.out.println("남은 기회는 " + count + "번입니다."); 
 		}
 	}
 	
-	public static int makeTargetNum() { // 원하는 숫자 클래스
-		int targetNum = (int)(Math.random() * 100) + 1; // 100개의 정수 중 하나를 랜덤 선정
+	public static int makeTargetNum() { 
+		int targetNum = (int)(Math.random() * 100) + 1; 
 		return targetNum;
 	}
 	
 	public static void judge(int targetNum, int userNum) { //
-		if (targetNum == userNum) { // 랜덤 숫자와 유저 숫자가 같을 때 True
+		if (targetNum == userNum) { 
 			System.out.println("정답");
-		} else if (targetNum > userNum) { // 유저 숫자보다 랜덤 숫자가 클 때
+		} else if (targetNum > userNum) { 
 			System.out.println(userNum + "보다 큽니다.");
-		} else { // 유저 숫자보다 랜덤 숫자가 작을 때
+		} else { 
 			System.out.println(userNum + "보다 작습니다."); 
 		}
 	}
 	
 	public static int inputData() {
-		System.out.print("입력:"); // 입력 출력
-		int num = scanner.nextInt(); // 
+		System.out.print("입력:"); 
+		int num = scanner.nextInt();
 		return num;
 	}
 	
-	// 시작 메시지(게임 규칙) 출력하기
 	public static void showRule() {
 		System.out.println("===== 1 ~ 100 사이의 임의의 수를 맞춰 보세요 =====");
 		System.out.println("----- 기회는 5번 입니다. -----");
@@ -74,15 +69,13 @@ public class GuessNumT {
 	 * 기록을 30000으로 초기화하기
 	 * @param records 점수 기록 배열
 	 */
-	
-	// 기록 초기화하기
+
 	public static void initRecords() {
 		for (int i = 0; i < records.length; i++) {
 			records[i] = 30000; // 
 		}
 	}
 	
-	// 기록 보여주기
 	public static void showRecords(int[] records) {
 		for (int aRecord : records) { // 읽기
 			System.out.print(aRecord + " ");
--- ch10/src/oc/OpenChallengeFrame.java
+++ ch10/src/oc/OpenChallengeFrame.java
@@ -1,28 +1,75 @@
 package oc;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Container;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
 public class OpenChallengeFrame extends JFrame {
 	Container con = getContentPane();
+	int index = 0;
+	JLabel lblImage = new JLabel(new ImageIcon("images/image0.jpg"));
+	ImageIcon[] iconImages = { new ImageIcon("images/image0.jpg"),
+							   new ImageIcon("images/image1.jpg"),
+							   new ImageIcon("images/image2.jpg"),
+							   new ImageIcon("images/image3.jpg")
+							 };
+	MenuPanel pnlMenu = new MenuPanel();
+	
 	
 	public OpenChallengeFrame() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setTitle("간단 이미지 뷰어");
-		con.setLayout(new BorderLayout());
-		JLabel lblCenter = new JLabel();
-		JButton btnLeft = new JButton();
-		JButton btnRight = new JButton();
-		
 		setSize(400, 500);
+		setUI();
 		setVisible(true);
 	}
+	
+	private void setUI() {
+		con.add(lblImage, BorderLayout.CENTER);
+		con.add(pnlMenu, BorderLayout.SOUTH);
+	}
+	
+	public class MenuPanel extends JPanel implements ActionListener {
+		private JButton btnLeft = new JButton(new ImageIcon("images/left.png"));
+		private JButton btnRight = new JButton(new ImageIcon("images/right.png"));
+		
+		public MenuPanel() {
+			add(btnLeft);
+			add(btnRight);
+			setBackground(Color.DARK_GRAY);
+			btnLeft.addActionListener(this);
+			btnRight.addActionListener(this);
+		}
 
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			Object obj = e.getSource();
+			if(obj == btnLeft) {
+				index--;
+				if(index > 0) {
+					
+				}
+			} else if(obj == btnRight) {
+				index++;
+				if(index < 0) {
+					
+				}
+			}
+			
+			
+		}
+		
+	}
+	
 	public static void main(String[] args) {
 		new OpenChallengeFrame();
 	}
